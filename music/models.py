9424609db43b9603a0f3616d95984cd25a0d00d7
Bertin19 <juniorwagueu@gmail.com>
2021-03-08 19:18:43 +0100

    Update workspace.xml

--- music/models.py
+++ music/models.py
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< Updated upstream
 from django.contrib.auth.models import Permission, User
 from django.db import models
 
@@ -32,6 +33,46 @@ from django.dispatch import receiver
 @receiver(user_logged_in)
 def retrieve_social_data(request, user, **kwargs):
     """Signal, that gets extra data from sociallogin and put it to profile."""
+=======
+=======
+>>>>>>> parent of edf2cf7... wip
+=======
+>>>>>>> Stashed changes
+from django.contrib.auth.models import Permission, User
+from django.db import models
+
+
+class Album(models.Model):
+    user = models.ForeignKey(User, default=1,on_delete=models.CASCADE)
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.FileField()
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.album_title + ' - ' + self.artist
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(upload_to="music")
+    is_favorite = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.song_title
+
+<<<<<<< HEAD
+
+from allauth.account.signals import user_logged_in
+from django.dispatch import receiver
+
+@receiver(user_logged_in)
+def retrieve_social_data(request, user, **kwargs):
+    """Signal, that gets extra data from sociallogin and put it to profile."""
+<<<<<<< Updated upstream
+=======
 =======
 =======
 >>>>>>> parent of edf2cf7... wip
@@ -68,6 +109,7 @@ from django.dispatch import receiver
 @receiver(user_logged_in)
 def retrieve_social_data(request, user, **kwargs):
     """Signal, that gets extra data from sociallogin and put it to profile."""
+>>>>>>> Stashed changes
 >>>>>>> 3c40d1c92f2fe860cd260a8d8e6510aef9f8ee90
     print(user)
 =======
